b30a76e424b6ece2ef36fcd1f7ae25ad6f2e506e
Cloud Dark <clouddark1798@gmail.com>
2023-12-29 14:03:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+const url = 'https://api.publicapis.org/entries';
+
+function showLoading() {
+  const dataTable = document.getElementById('data-table');
+  const loadingMessage = document.getElementById('loading-message');
+  dataTable.style.display = 'none';
+  loadingMessage.style.display = 'block';
+}
+
+function hideLoading() {
+  const loadingMessage = document.getElementById('loading-message');
+  loadingMessage.style.display = 'none';
+}
+
+function fetchData() {
+  const dataTable = document.getElementById('data-table');
+  const loadingMessage = document.getElementById('loading-message');
+  dataTable.style.display = 'none';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const count = data.count;
+      const entries = data.entries;
+      const dataCountElement = document.getElementById('data-count');
+      const dataTableBody = document.getElementById('data-body');
+
+      dataCountElement.textContent = `Jumlah data: ${count}`;
+      dataTableBody.innerHTML = '';
+
+      entries.forEach(entry => {
+        const row = document.createElement('tr');
+
+        const titleCell = document.createElement('td');
+        titleCell.textContent = entry.API;
+        row.appendChild(titleCell);
+
+        const descriptionCell = document.createElement('td');
+        descriptionCell.textContent = entry.Description;
+        row.appendChild(descriptionCell);
+
+        const linkCell = document.createElement('td');
+        const linkAnchor = document.createElement('a');
+        linkAnchor.href = entry.Link;
+        linkAnchor.textContent = entry.Link;
+        linkCell.appendChild(linkAnchor);
+        row.appendChild(linkCell);
+
+        const categoryCell = document.createElement('td');
+        categoryCell.textContent = entry.Category;
+        row.appendChild(categoryCell);
+        dataTableBody.appendChild(row);
+        dataTable.style.display = 'block';
+      });
+
+      hideLoading();
+    })
+    .catch(error => {
+      hideLoading();
+      console.error('Error:', error);
+    });
+}
+
+fetchData();
